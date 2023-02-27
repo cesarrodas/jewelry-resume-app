6f93a6fc595ecef3195d414d3aab1a3b5f86ecf1
cesar rodas <rodascesar03@yahoo.com>
2023-02-27 15:45:24 -0600

    2 panel images added and title content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { DividerComponent } from './divider/divider.component';
 import { PanelsComponent } from './panels/panels.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FooterComponent } from './footer/footer.component';
     DividerComponent,
     PanelsComponent,
     ProductsComponent,
-    FooterComponent
+    FooterComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
