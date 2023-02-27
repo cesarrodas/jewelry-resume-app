c046cb4b250438005ba05e5bebdacb9d406142e8
cesar rodas <rodascesar03@yahoo.com>
2023-02-27 17:46:43 -0600

    Added product pictures

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-
+  products = [
+    {
+      name: "Bracelet",
+      price: "$225",
+      img: "./assets/item-01.webp"
+    },
+    {
+      name: "Twisted Metal",
+      price: "$200",
+      img: "./assets/item-02.webp"
+    },
+    {
+      name: "Heart Chain",
+      price: "$330",
+      img: "./assets/item-03.webp"
+    },
+    {
+      name: "Heart Pendant",
+      price: "$165",
+      img: "./assets/item-04.webp"
+    }
+  ]
 }
