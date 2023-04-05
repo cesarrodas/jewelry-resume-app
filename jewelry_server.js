a2c4757cdef4d75efcf4707dd84ef5765d37b336
cesar rodas <rodascesar03@yahoo.com>
2023-04-05 14:11:41 -0500

    changed server

--- jewelry_server.js
+++ jewelry_server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 4021;
 
-app.use(express.static('dist'));
+app.use(express.static('dist/jewelry-resume-app'));
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
